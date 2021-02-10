7a40805f993ddb0cf58636a5a308f015eae87c04
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-02-10 17:36:15 +0200

    RSS list screen visual list with styles added.

--- screens/RSSListScreen/RSSListScreen.js
+++ screens/RSSListScreen/RSSListScreen.js
@@ -1,27 +1,90 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const RSSListScreen = props => {
+  const [rssList, setRssList] = useState([
+    {
+      id: '1',
+      image: '',
+      date: '08.02.2020',
+      title: 'Some old stuff',
+    },
+    {
+      id: '2',
+      image: '',
+      date: '09.02.2020',
+      title: 'Something happened today',
+    },
+    {
+      id: '3',
+      image: '',
+      date: '10.02.2020',
+      title: 'Breaking news',
+    },
+  ]);
+
+  const renderRSSListItem = itemData => {
     return (
-        <View style={styles.headerContainer}>
-            <Text style={styles.headerText}>RSS Feed</Text>
-        </View>
+      <View style={styles.rssListItem}>
+        <Text style={styles.rssListItemDate}>{itemData.item.date}</Text>
+        <Text style={styles.rssListItemTitle}>{itemData.item.title}</Text>
+      </View>
     );
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>RSS Feed</Text>
+      </View>
+      <FlatList
+        data={rssList}
+        keyExtractor={item => item.id}
+        renderItem={item => renderRSSListItem(item)}
+        style={styles.rssList}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    headerContainer: {
-        height: 90,
-        justifyContent: 'center',
-        backgroundColor: 'cornflowerblue',
-        paddingTop: 35,
-    },
-    headerText: {
-        textAlign: 'center',
-        color: 'white',
-        fontWeight: 'bold',
-        fontSize: 20
-    }
+  screen: {
+    flex: 1,
+  },
+  headerContainer: {
+    justifyContent: 'center',
+    height: 90,
+    paddingTop: 35,
+    backgroundColor: 'cornflowerblue',
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  rssList: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    backgroundColor: 'aliceblue'
+  },
+  rssListItem: {
+    justifyContent: 'space-evenly',
+    height: 60,
+    width: '100%',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: '#C3DEF4'
+  },
+  rssListItemDate: {
+    fontSize: 12,
+    color: 'dimgray',
+  },
+  rssListItemTitle: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
 });
 
 export default RSSListScreen;
