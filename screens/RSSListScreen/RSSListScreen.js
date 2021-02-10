5662dc5d4d7f5f88a172bc054d94a0fdbe3b9c18
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-02-10 14:39:07 +0200

    Initial setup. RSSListScreen added.

---
+++ screens/RSSListScreen/RSSListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const RSSListScreen = props => {
+    return (
+        <View>
+            <Text>RSSListScreen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default RSSListScreen;
