417f8f2f5a38ab8619188ee27daeb566502ad406
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-02-10 15:12:02 +0200

    RSS List Screen header added.

--- screens/RSSListScreen/RSSListScreen.js
+++ screens/RSSListScreen/RSSListScreen.js
@@ -3,12 +3,25 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const RSSListScreen = props => {
     return (
-        <View>
-            <Text>RSSListScreen</Text>
+        <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>RSS Feed</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    headerContainer: {
+        height: 90,
+        justifyContent: 'center',
+        backgroundColor: 'cornflowerblue',
+        paddingTop: 35,
+    },
+    headerText: {
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20
+    }
+});
 
 export default RSSListScreen;
