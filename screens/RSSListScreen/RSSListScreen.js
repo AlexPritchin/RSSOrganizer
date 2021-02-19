ca1f29f976e76a868cd360e0666cc0f99bdd1c6c
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-02-19 18:32:23 +0200

    RSS dummy data changed.

--- screens/RSSListScreen/RSSListScreen.js
+++ screens/RSSListScreen/RSSListScreen.js
@@ -7,28 +7,37 @@ const RSSListScreen = props => {
   const [rssList, setRssList] = useState([
     {
       id: '1',
-      image: '',
-      date: '08.02.2020',
-      title: 'Some old stuff',
+      publicationDate: 'Fri, 19 Feb 2021 16:09:34 +0000',
+      title: 'Von Trapped: The Family Is Stuck Inside, So Why Not Sing Parodies?',
+      description: 'The Marshes, an ordinary English family, have gained extraordinary fame for their musical spoofs of lockdown life. And no, they are not trying to be the von Trapps.',
+      creator: 'Isabella Kwai',
+      link: 'https://www.nytimes.com/2021/02/19/world/europe/virus-YouTube-Marsh-family.html',
+      imageLink: 'https://static01.nyt.com/images/2021/02/18/world/18marsh/18marsh-moth.jpg',
     },
     {
       id: '2',
-      image: '',
-      date: '09.02.2020',
-      title: 'Something happened today',
+      publicationDate: 'Fri, 19 Feb 2021 15:47:44 +0000',
+      title: 'Russia Is Offering to Export Hundreds of Millions of Vaccine Doses, but Can It Deliver?',
+      description: 'The Kremlin has scored propaganda points and bolstered several longstanding foreign policy goals by offering its Sputnik V vaccine around the world. But it has limited production capacity.',
+      creator: 'Andrew E. Kramer',
+      link: 'https://www.nytimes.com/2021/02/19/world/europe/russia-coronavirus-vaccine-soft-power.html',
+      imageLink: 'https://static01.nyt.com/images/2021/02/18/world/00virus-russia1/merlin_183917829_3260652d-efa6-4272-99ed-d9d599eddf94-moth.jpg',
     },
     {
       id: '3',
-      image: '',
-      date: '10.02.2020',
-      title: 'Breaking news',
+      publicationDate: 'Fri, 19 Feb 2021 15:48:29 +0000',
+      title: 'Don Letts, Mad Professor Team With Times on Carnival Story ',
+      description: 'The pandemic has dampened the celebrations worldwide. But a Times special project, which includes an interactive music mixing feature, lets readers get into a party mood.',
+      creator: 'Adam Sternbergh',
+      link: 'https://www.nytimes.com/2021/02/19/insider/carnival-music-mix.html',
+      imageLink: 'https://static01.nyt.com/images/2021/02/19/insider/19insider-carnival/19insider-carnival-moth.jpg',
     },
   ]);
 
   const renderRSSListItem = itemData => {
     return (
       <View style={styles.rssListItem}>
-        <Text style={styles.rssListItemDate}>{itemData.item.date}</Text>
+        <Text style={styles.rssListItemDate}>{itemData.item.publicationDate}</Text>
         <Text style={styles.rssListItemTitle}>{itemData.item.title}</Text>
       </View>
     );
