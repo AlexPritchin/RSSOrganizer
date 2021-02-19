06c2dbc6bd69ee27f93b23a1c469fd19cac15f9d
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-02-19 20:06:24 +0200

    Stack navigators header font size increased on iOS.

--- constants/GlobalNavigationOptions.js
+++ constants/GlobalNavigationOptions.js
@@ -1,8 +1,13 @@
+import { Platform } from 'react-native';
+
 import Colors from './Colors';
 
 const defaultStackNavigationOptions = {
     headerStyle: {
         backgroundColor: Colors.backgroundHeaderColor
+    },
+    headerTitleStyle: {
+        fontSize: Platform.OS === 'ios' ? 19 : 20
     },
     headerTintColor: Colors.headerTextColor
 };
--- screens/OrganizerListScreen/OrganizerListScreen.js
+++ screens/OrganizerListScreen/OrganizerListScreen.js
@@ -10,8 +10,34 @@ const OrganizerListScreen = props => {
   );
 };
 
-const styles = StyleSheet.create({
-  
-});
+// const styles = StyleSheet.create({
+//   rssList: {
+//     paddingHorizontal: 20,
+//     paddingTop: 20,
+//     backgroundColor: Colors.backgroundScreenColor
+//   },
+//   rssListItemContainer: {
+//     flexDirection: 'row',
+//     height: 60,
+//     width: '100%',
+//   },
+//   rssListItemTextContainer: {
+//     justifyContent: 'space-evenly',
+//     height: 60,
+//     width: '100%',
+//     marginBottom: 10,
+//     padding: 10,
+//     borderRadius: 10,
+//     backgroundColor: Colors.backgroundListItemColor
+//   },
+//   rssListItemDate: {
+//     fontSize: 12,
+//     color: Colors.listItemTitleColor,
+//   },
+//   rssListItemTitle: {
+//     fontSize: 14,
+//     fontWeight: 'bold'
+//   },
+// });
 
 export default OrganizerListScreen;
