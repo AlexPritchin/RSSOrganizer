00553409e43727aec1a9c32d0bb5ab887097811a
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-03-05 10:25:31 +0200

    React native screens use added.

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { enableScreens } from 'react-native-screens';
 
 import MainNavigator from './navigation/MainTabNavigator';
+
+enableScreens();
 
 export default function App() {
   return <MainNavigator />;
