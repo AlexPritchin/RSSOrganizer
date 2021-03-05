9eb896bd9887105bb71b73e00b456b96fa62913e
Oleksiy Prytchyn <alpritchin@ukr.net>
2021-03-05 13:15:22 +0200

    Redux store initial setup added.

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { enableScreens } from 'react-native-screens';
 import { LogBox } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MainNavigator from './navigation/MainTabNavigator';
+import { organizerReducer } from './store/reducers/OrganizerReducer';
 
 enableScreens();
+
+const rootReducer = combineReducers({
+  organizerTasks: organizerReducer,
+});
+
+const globalStore = createStore(rootReducer);
 
 export default function App() {
   LogBox.ignoreLogs([
     'It appears that you are using old version of react-navigation library',
-    'Deprecation in \'navigationOptions\''
+    'Deprecation in \'navigationOptions\'',
   ]);
-  return <MainNavigator />;
+  return (
+    <Provider store={globalStore}>
+      <MainNavigator />
+    </Provider>
+  );
 }
